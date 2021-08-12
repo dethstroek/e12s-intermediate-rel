db29195aff1eb8c0068c27aa3d0e3e56da969561
Dethstroek <dethstroekttv@gmail.com>
2021-08-12 13:21:52 -0400

    Update e12s.js

--- e12s.js
+++ e12s.js
@@ -3,3 +3,18 @@ document.getElementById("game").appendChild(application.game);
 
 var container = new PIXI.Container();
 application.stage.addChild(container);
+
+var state = {};
+var textures = {};
+var elems = {};
+
+function loadTextures(){
+    //Need background arena texture
+    //Need boss texture
+    textures.status.aero = PIXI.Texture.fromImage('img/aero.png');
+    textures.status.flare = PIXI.Texture.fromImage('img/flare.png');
+    textures.status.ice = PIXI.Texture.fromImage('img/ice.png');
+    textures.status.gaze = PIXI.Texture.fromImage('img/gaze.png');
+    textures.status.stack = PIXI.Texture.fromImage('img/stack.png');
+    textures.status.return = PIXI.Texture.fromImage('img/return.png');
+}
