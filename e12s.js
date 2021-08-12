cfe2eefcb2029348ad5751fc3c9f4bc4a4264d8a
Dethstroek <dethstroekttv@gmail.com>
2021-08-12 12:50:49 -0400

    Update e12s.js

--- e12s.js
+++ e12s.js
@@ -1,5 +1,5 @@
 var application = new PIXI.Application(600, 700, { backgroundColor: 0x1099bb });
-document.getElementById("view").appendChild(application.view);
+document.getElementById("game").appendChild(application.view);
 
 var container = new PIXI.Container();
 application.stage.addChild(container);
